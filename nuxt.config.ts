841cd5af4eb7e75161a0def7f48678bbf9ee5161
jlemonz <112578448+jlemonz@users.noreply.github.com>
2025-09-04 09:54:29 +0200

    s

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,10 @@
 export default defineNuxtConfig({
   routeRules: {
-    // revalidated every 60 seconds, in the background
-    '/**': { isr: 60 },
-    // this page will be always fresh
-    '/dynamic': { isr: false },
-    // this page will be generated on demand and then cached permanently
-    '/static': { isr: true },
-    // this page is generated at build time and cached permanently
-    '/prerendered': { prerender: true },
-    // you can do lots more with route rules too!
-    '/redirect': { redirect: '/static' },
-    '/headers': { headers: { 'x-magic-of': 'nuxt and vercel' } },
-    '/spa': { ssr: false },
+    // all routes (by default) will be revalidated every 60 seconds, in the background
+    "/**": { isr: 60 },
   },
   runtimeConfig: {
-    nitro: { envPrefix: 'VERCEL_' },
+    nitro: { envPrefix: "VERCEL_" },
     region: process.env.VERCEL_REGION,
   },
-})
+});
